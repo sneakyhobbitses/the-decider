2d84bc901397673ef2b49f670177bcbaa0ebad61
Sarah <sarahkrysten@hotmail.com>
2022-04-07 23:33:54 -0400

    cleaned up boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './UserInput';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>
+        <p>henlo</p>
+        <p>this is where we will make decisions</p>
+        <p>how exciting</p>
+        <p><UserInput /></p>
+      </span>
+    
     </div>
   );
 }
